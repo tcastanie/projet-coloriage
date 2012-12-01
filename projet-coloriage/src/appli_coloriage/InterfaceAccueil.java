531aa7a0dc671afd11dd6c85fa75088d7d900121
thibaut.castanie@gmail.com <thibaut.castanie@gmail.com@03bbea82-9b38-e805-60e1-0c5059fe23bc>
2012-12-01 22:21:58 +0000

    Amélioration de la gestion mémoire des fenêtres. Restructuration de l'architecture du projet.

---
+++ projet-coloriage/src/appli_coloriage/InterfaceAccueil.java
@@ -0,0 +1,15 @@
+package appli_coloriage;
+
+
+public class InterfaceAccueil {
+	
+
+	public static Fenetre fenetreBasique;
+
+	public static void main(String[] args){
+		   
+			fenetreBasique = new Fenetre();
+			fenetreBasique.setVisible(true);
+	      
+	   }  
+}
